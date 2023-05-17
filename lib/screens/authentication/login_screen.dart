3fb29366295adc1a114292607814401ec4ff13a2
mac <abdogray@gmail.com>
2023-05-17 21:53:32 +0100

    clean up

--- lib/screens/authentication/login_screen.dart
+++ lib/screens/authentication/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:dom_sys/screens/authentication/forget_password.dart';
+import 'package:dom_sys/screens/authentication/signup_screen.dart';
 import 'package:dom_sys/widgets/rounded_button.dart';
 import 'package:dom_sys/widgets/social_login_button%20.dart';
 import 'package:flutter/material.dart';
@@ -7,6 +8,7 @@ import '../../widgets/custom_text12.dart';
 import '../../widgets/custom_text14.dart';
 import '../../widgets/custom_text16.dart';
 import '../../widgets/custom_textformfield.dart';
+import '../doctor/doctor_dashboard_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -14,8 +16,107 @@ class LoginScreen extends StatefulWidget {
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                _header(),
+                const SizedBox(
+                  height: 24,
+                ),
+                _formField(),
+             const   SizedBox(
+                  height: 24,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Checkbox(
+                            checkColor: Colors.white,
+                            // fillColor: MaterialStateProperty.resolveWith(Colors.white),
+                            value: false, //isChecked,
+                            onChanged: (bool? value) {
+                              setState(() {
+                                //   isChecked = value!;
+                              });
+                            }),
+                        const CustomText12(text: "Remember me"),
+                      ],
+                    ),
+                    GestureDetector(
+                        onTap: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const ForgetPassword(),
+                              ),
+                            ),
+                        child: const CustomText12(text: "Forgot password?")),
+                  ],
+                ),
+               const  SizedBox(
+                  height: 24,
+                ),
+                RoundedButton(
+                    text: "Sign In",
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const DashboardScreen(),
+                        ),
+                      );
+                    }),
+             const    SizedBox(
+                  height: 24,
+                ),
+                const Text(
+                  "Or",
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+           const      SizedBox(
+                  height: 24,
+                ),
+                _socialLogin(),
+            const     SizedBox(
+                  height: 24,
+                ),
+                GestureDetector(
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SignUpScreen(),
+                    ),
+                  ),
+                  child: const Text.rich(
+                    TextSpan(
+                      text: 'Didn’t have an account ?',
+                      children: [
+                        TextSpan(
+                          text: 'Sign Up',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
-Widget _header() => Column(
+  Widget _header() => Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Row(
@@ -80,86 +181,4 @@ Widget _socialLogin() => Column(
       ],
     );
 
-class _LoginScreenState extends State<LoginScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                _header(),
-                const SizedBox(
-                  height: 24,
-                ),
-                _formField(),
-                SizedBox(
-                  height: 24,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        Checkbox(
-                            checkColor: Colors.white,
-                            // fillColor: MaterialStateProperty.resolveWith(Colors.white),
-                            value: false, //isChecked,
-                            onChanged: (bool? value) {
-                              setState(() {
-                                //   isChecked = value!;
-                              });
-                            }),
-                        const CustomText12(text: "Remember me"),
-                      ],
-                    ),
-                    GestureDetector(
-                          onTap: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const ForgetPassword(),
-                    ),
-                  ),
-                      child: const CustomText12(text: "Forgot password?")),
-                  ],
-                ),
-                SizedBox(
-                  height: 24,
-                ),
-                RoundedButton(text: "Sign Up", onPressed: () {}),
-                SizedBox(
-                  height: 24,
-                ),
-                const Text(
-                  "Or",
-                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  height: 24,
-                ),
-                _socialLogin(),
-                SizedBox(
-                  height: 24,
-                ),
-                const Text.rich(
-                  TextSpan(
-                    text: 'Didn’t have an account ?',
-                    children: [
-                      TextSpan(
-                        text: 'Sign Up',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
 }
