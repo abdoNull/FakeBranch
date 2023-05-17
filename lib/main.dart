e50b5009ee196b642bd231829edf28e0df2fc757
mac <abdogray@gmail.com>
2023-05-17 21:32:45 +0100

    clean up

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,5 @@ import 'package:device_preview/device_preview.dart';
 import 'dom_sys_app.dart';
 
 void main() {
-  runApp(DevicePreview(enabled: true, builder: (context) => const DomSysApp()));
+  runApp(DevicePreview(enabled: false, builder: (context) => const DomSysApp()));
 }
